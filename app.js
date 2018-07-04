3d30112fa5c33584bd334cfc45f6961d6bbd83d3
scriptype <scriptyper@gmail.com>
2018-07-04 20:34:24 +0300

    Add masthead video buttons

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const nextBtn = document.getElementById('masthead-next-video-button')
 nextBtn.addEventListener('click', e => {
   const activeVideo = document.querySelector('.masthead__video--active')
   let nextVideo = activeVideo.nextElementSibling
-  if (nextVideo.nodeName !== 'VIDEO') {
+  if (!nextVideo) {
     nextVideo = activeVideo.parentNode.querySelector('.masthead__video')
   }
   nextVideo.classList.add(activeClass)
@@ -17,7 +17,7 @@ const prevBtn = document.getElementById('masthead-prev-video-button')
 prevBtn.addEventListener('click', e => {
   const activeVideo = document.querySelector('.masthead__video--active')
   let prevVideo = activeVideo.previousElementSibling
-  if (!prevVideo || prevVideo.nodeName !== 'VIDEO') {
+  if (!prevVideo) {
     prevVideo = last(activeVideo.parentNode.querySelectorAll('.masthead__video'))
   }
   prevVideo.classList.add(activeClass)
