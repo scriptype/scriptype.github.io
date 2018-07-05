adb4d97e7c3ec4d4fb45c0490b9c5cdd0a34acc7
scriptype <scriptyper@gmail.com>
2018-07-05 19:09:51 +0300

    Add works section

--- app.js
+++ app.js
@@ -23,3 +23,37 @@ prevBtn.addEventListener('click', e => {
   prevVideo.classList.add(activeClass)
   activeVideo.classList.remove(activeClass)
 })
+
+
+const workButtons = document.querySelectorAll('[data-work-button]')
+const workContainers = document.querySelectorAll('[data-work-container]')
+workButtons.forEach(button => {
+  const container = Array.from(workContainers).find(container => {
+    return container.dataset.workContainer === button.dataset.workButton
+  })
+  button.addEventListener('click', e => {
+    workButtons.forEach(btn => btn.classList.remove('work-button--active'))
+    workContainers.forEach(con => con.classList.remove('work-container--active'))
+    button.classList.add('work-button--active')
+    container.classList.add('work-container--active')
+  })
+})
+
+const changeWorkButtons = document.querySelectorAll('[data-change-work-button]')
+const works = document.querySelectorAll('[data-work]')
+changeWorkButtons.forEach(button => {
+  button.addEventListener('click', e => {
+    const activeWork = document.querySelector('.work-container__work--active')
+    const prevWork = activeWork.previousElementSibling
+    const nextWork = activeWork.nextElementSibling
+    works.forEach(work => {
+      work.classList.remove('work-container__work--active')
+    })
+    const isNext = button.dataset.changeWorkButton === 'next'
+    if (isNext) {
+      nextWork.classList.add('work-container__work--active')
+    } else {
+      prevWork.classList.add('work-container__work--active')
+    }
+  })
+})
