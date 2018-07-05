eb66b17e029f6c658825b9e448fdd259cae70198
scriptype <scriptyper@gmail.com>
2018-07-06 01:34:59 +0300

    Remove masthead videos (only one left, unchangable)
    - remove bg from about me section
    - fix theme parameter of moon css'd

--- app.js
+++ app.js
@@ -1,30 +1,5 @@
 const last = array => array[array.length - 1]
 
-const activeClass = 'masthead__video--active'
-
-const nextBtn = document.getElementById('masthead-next-video-button')
-nextBtn.addEventListener('click', e => {
-  const activeVideo = document.querySelector('.masthead__video--active')
-  let nextVideo = activeVideo.nextElementSibling
-  if (!nextVideo) {
-    nextVideo = activeVideo.parentNode.querySelector('.masthead__video')
-  }
-  nextVideo.classList.add(activeClass)
-  activeVideo.classList.remove(activeClass)
-})
-
-const prevBtn = document.getElementById('masthead-prev-video-button')
-prevBtn.addEventListener('click', e => {
-  const activeVideo = document.querySelector('.masthead__video--active')
-  let prevVideo = activeVideo.previousElementSibling
-  if (!prevVideo) {
-    prevVideo = last(activeVideo.parentNode.querySelectorAll('.masthead__video'))
-  }
-  prevVideo.classList.add(activeClass)
-  activeVideo.classList.remove(activeClass)
-})
-
-
 const worksButtons = document.querySelectorAll('[data-works-button]')
 const workContainers = document.querySelectorAll('[data-work-container]')
 worksButtons.forEach(button => {
