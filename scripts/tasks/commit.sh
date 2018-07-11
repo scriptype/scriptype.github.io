55ce675071cb8ae3b431562bdda025e40e2b3d42
scriptype <scriptyper@gmail.com>
2018-07-11 03:38:49 +0300

    Add post-commit hook to run build script automatically

--- scripts/tasks/commit.sh
+++ scripts/tasks/commit.sh
@@ -1,1 +1,10 @@
+gitdir="$(git rev-parse --git-dir)"
+hook="$gitdir/hooks/post-commit"
+
+# disable post-commit hook temporarily
+[ -x $hook ] && chmod -x $hook
+
 git commit -am "Build"
+
+# enable it again
+chmod +x $hook
